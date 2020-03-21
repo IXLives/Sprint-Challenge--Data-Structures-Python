99ca0956c7e10a584696bc5ee199e1348151b8d9
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-03-21 15:43:42 -0400

    At one point my ring buffer was passing until we added f, now I have ruined it

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,8 +4,8 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
         self.storage = DoublyLinkedList()
+        self.current = self.storage.head
 
     def append(self, item):
         # check if ring is full (tail = capacity - 1 And front == 0)
@@ -17,13 +17,20 @@ class RingBuffer:
         if (size == self.capacity):
             self.storage.remove_from_head()
             self.storage.add_to_tail(item)
+            if self.current == self.storage.head:
+                self.current = self.current.next
+            self.current = self.storage.tail
+
         # check if empty
         elif (self.storage.length == 0):
             self.storage.add_to_tail(item)
             size += 1
+            self.current = self.storage.head
+
         else:
             self.storage.add_to_tail(item)
             size += 1
+            self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -31,10 +38,19 @@ class RingBuffer:
         # TODO: Your code here
         # from head of list, add items to list buffer
         # if next item is none, stop adding
-        self.current = self.storage.head
-        while self.current != None:
+        start = self.current
+        if start.next is not None:
+            next_node = start.next
+        elif start.next is None:
+            next_node = self.storage.head
+        while next_node != start:
+            if next_node == None:
+                next_node = self.storage.head
             list_buffer_contents.append(self.current.value)
             self.current = self.current.next
+            if self.current.next == None:
+                next_node = self.storage.head
+
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
